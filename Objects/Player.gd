ab458818c4ee2177ae64e596a506ab1e6c92c79d
lasive <109923383+lasive@users.noreply.github.com>
2023-02-04 13:31:40 +0200

    eye

--- Objects/Player.gd
+++ Objects/Player.gd
@@ -5,6 +5,7 @@ export var maxSpeedY = 5000
 export var maxSpeedX = 300
 export var jump_speed = -300
 export var gravity = 1000
+var directionX = 1
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -19,18 +20,22 @@ func get_input(delta):
 	
 	if abs(velocity.x) < 50:
 		velocity.x = 0
+	else:
+		directionX = sign(velocity.x)
 	if Input.is_action_pressed("move_right"):
 		velocity.x += speed
 	elif Input.is_action_pressed("move_left"):
 		velocity.x -= speed
 	else:
 		velocity.x -= sign(velocity.x)*speed
-	
 	if abs(velocity.x) > maxSpeedX:
 		velocity.x = sign(velocity.x)*maxSpeedX
 		
 func _physics_process(delta: float) -> void:
 	get_input(delta)
+	
+	$letterE_L.hide()
+	$letterE_R.hide()
 	
 	velocity.y += gravity * delta
 	velocity.y = min(velocity.y, maxSpeedY)
@@ -43,12 +48,29 @@ func _physics_process(delta: float) -> void:
 		elif  is_on_wall():
 			velocity.y = jump_speed
 	move_and_slide(velocity,Vector2.UP)
-	
-	if Input.is_action_just_pressed("left_click"):
-		print(0)
-		var mouse_pos = get_viewport().get_mouse_position()
+	if is_on_floor():
 		var tilemap = get_parent().get_node("TileMap")
-		var tile_pos = tilemap.world_to_map(mouse_pos)
+		var aproximate_position = Vector2(global_position.x + directionX * 64, global_position.y + 32)
+		var tile_pos = tilemap.world_to_map(aproximate_position)
 		var cell = tilemap.get_cellv(tile_pos)
-		print(tile_pos)
-		print(cell)
+		if cell == 0:
+			
+			if Input.is_action_just_pressed("E"):
+				get_parent().plant_seed(aproximate_position)
+				print("ok")
+			#print(cell)
+			if directionX >= 0:
+				$letterE_R.show()
+			else:
+				$letterE_L.show()
+
+		
+		
+#	if Input.is_action_just_pressed("left_click"):
+#		print(0)
+#		var mouse_pos = get_global_mouse_position()
+#		var tilemap = get_parent().get_node("TileMap")
+#		var tile_pos = tilemap.world_to_map(mouse_pos)
+#		var cell = tilemap.get_cellv(tile_pos)
+#		print(tile_pos)
+#		print(cell)
