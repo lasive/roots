a78efc0e83e8c2228838306c6537aae0968a5227
lasive <109923383+lasive@users.noreply.github.com>
2023-02-04 12:01:18 +0200

    fix

--- HUD/HUD.gd
+++ HUD/HUD.gd
@@ -2,12 +2,12 @@ extends Node2D
 
 
 
-func _on_StartButton_pressed():
-	get_tree().change_scene("res://MainScene.tscn")
-
-
 
 
 func _on_BackButton_pressed():
 	get_tree().quit()
 	
+
+
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://MainScene.tscn")
