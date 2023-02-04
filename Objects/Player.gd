93d03d82d4164fb58f8f59740bf0cdb4fe608b4b
lasive <109923383+lasive@users.noreply.github.com>
2023-02-04 14:14:13 +0200

    mainscene map, collision boxes

--- Objects/Player.gd
+++ Objects/Player.gd
@@ -1,10 +1,10 @@
 extends KinematicBody2D
 var velocity = Vector2.ZERO
-export var speed = 50
-export var maxSpeedY = 5000
-export var maxSpeedX = 300
-export var jump_speed = -300
-export var gravity = 1000
+export var speed = 20
+export var maxSpeedY = 1000
+export var maxSpeedX = 100
+export var jump_speed = -100
+export var gravity = 500
 var directionX = 1
 # Declare member variables here. Examples:
 # var a = 2
@@ -18,7 +18,7 @@ func _ready():
 
 func get_input(delta):
 	
-	if abs(velocity.x) < 50:
+	if abs(velocity.x) < 10:
 		velocity.x = 0
 	else:
 		directionX = sign(velocity.x)
@@ -50,7 +50,7 @@ func _physics_process(delta: float) -> void:
 	move_and_slide(velocity,Vector2.UP)
 	if is_on_floor():
 		var tilemap = get_parent().get_node("TileMap")
-		var aproximate_position = Vector2(global_position.x + directionX * 64, global_position.y + 32)
+		var aproximate_position = Vector2(global_position.x + directionX * 8, global_position.y + 4)
 		var tile_pos = tilemap.world_to_map(aproximate_position)
 		var cell = tilemap.get_cellv(tile_pos)
 		if cell == 0:
