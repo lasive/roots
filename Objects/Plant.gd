0d28b0a0a556f2a1d9b6919a9c89c195bbeaa3e0
lasive <109923383+lasive@users.noreply.github.com>
2023-02-04 22:15:02 +0200

    groth

--- Objects/Plant.gd
+++ Objects/Plant.gd
@@ -18,18 +18,23 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("left_click"):
-		growing = true
-		draw_line(current_stem_pos_start, mouse_pos,Color.black, 50, true)
-		print(current_stem_pos_start)
-		print(mouse_pos)
-	if Input.is_action_just_released("left_click"):
-		growing = false
-	if growing:
-		pass
+		
+		var mouse_pos = get_global_mouse_position()
+		var tilemap = get_node("TileMap")
+		var tile_pos = tilemap.world_to_map(mouse_pos - tilemap.global_position)
+		var cell = tilemap.get_cellv(tile_pos)
+		print(cell)
+		tilemap.set_cellv(tile_pos, 0)
+		tilemap.update_bitmask_region()
+#		print(cell)
+	#if Input.is_action_just_released("left_click"):
+	#	growing = false
+#	if growing:
+#		pass
 	
-func _input(event):
-	if event is InputEventMouseMotion:
-		mouse_pos = get_global_mouse_position()
-		current_stem_pos_start = current_stem_pos_end
+#func _input(event):
+#	if event is InputEventMouseMotion:
+#		mouse_pos = get_global_mouse_position()
+#		current_stem_pos_start = current_stem_pos_end
 
 	
